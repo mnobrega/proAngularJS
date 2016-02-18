6561dca63dd3c6378a3526d496754c8907cba7db
Marcio nobrega <marcio.nobrega@sapo.pt>
2016-02-18 01:08:39 +0000

    Added gulp stuff.

---
+++ gulp/build.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+
+gulp.task("build",function(){
+    gulp.src("bower_components/jquery/dist/jquery.js").pipe(gulp.dest("dist/js/"));
+    gulp.src("bower_components/angular/angular.js").pipe(gulp.dest("dist/js/"));
+    gulp.src("bower_components/bootstrap/dist/css/**").pipe(gulp.dest("dist/css/"));
+    gulp.src("bower_components/bootstrap/dist/js/bootstrap.js").pipe(gulp.dest("dist/js/"));
+    gulp.src("bower_components/bootstrap/dist/fonts/**").pipe(gulp.dest("dist/fonts/"));
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task("copyfiles",function(){
-    gulp.src("bower_components/jquery/dist/jquery.js").pipe(gulp.dest("dist/js/"));
-    gulp.src("bower_components/angular/angular.js").pipe(gulp.dest("dist/js/"));
-    gulp.src("bower_components/bootstrap/dist/css/**").pipe(gulp.dest("dist/css/"));
-    gulp.src("bower_components/bootstrap/dist/js/bootstrap.js").pipe(gulp.dest("dist/js/"));
-    gulp.src("bower_components/bootstrap/dist/fonts/**").pipe(gulp.dest("dist/fonts/"));
-});
+var gulp = require('gulp');
+require('require-dir')('./gulp');
+
+gulp.paths = {
+    src: 'src',
+    dist: 'dist',
+    tmp: '.tmp'
+};
 
 gulp.task("default",function(){
-    gulp.start("copyfiles");
+    gulp.start("build");
 });
