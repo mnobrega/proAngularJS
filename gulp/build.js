9314051cdaae7ef9dbab5c90d78fbe3af82f802f
Marcio Nobrega <marcio.nobrega@sapo.pt>
2016-03-09 01:58:19 +0000

    Added more gulp tasks for fonts and assets.

--- gulp/build.js
+++ gulp/build.js
@@ -6,14 +6,27 @@ var $ = require('gulp-load-plugins')({
     pattern: ['gulp-*','main-bower-files','del']
 });
 
-gulp.task("build",function(){
-    gulp.src("bower_components/jquery/dist/jquery.js").pipe(gulp.dest("dist/js/"));
-    gulp.src("bower_components/angular/angular.js").pipe(gulp.dest("dist/js/"));
-    gulp.src("bower_components/bootstrap/dist/css/**").pipe(gulp.dest("dist/css/"));
-    gulp.src("bower_components/bootstrap/dist/js/bootstrap.js").pipe(gulp.dest("dist/js/"));
-    gulp.src("bower_components/bootstrap/dist/fonts/**").pipe(gulp.dest("dist/fonts/"));
+//gulp.task("build",function(){
+//    gulp.src("bower_components/jquery/dist/jquery.js").pipe(gulp.dest("dist/js/"));
+//    gulp.src("bower_components/angular/angular.js").pipe(gulp.dest("dist/js/"));
+//    gulp.src("bower_components/bootstrap/dist/js/bootstrap.js").pipe(gulp.dest("dist/js/"));
+//    gulp.src("bower_components/bootstrap/dist/css/**").pipe(gulp.dest("dist/css/"));
+//});
+
+gulp.task('i18n',function() {
+    //send to temp as it needs to be compiled
 });
 
-gulp.task("clean",function(done) {
-    $.del([paths.dist+"/",paths.tmp+"/"],done);
+gulp.task('images',function(){
+    return gulp.src(paths.src+'/assets/img/**/*').pipe(gulp.dest(paths.dist+'/img/'));
 });
+
+gulp.task('bootstrapfonts',function() {
+    return gulp.src('bower_components/bootstrap/fonts/*.{eot,svg,ttf,woff,woff2}').pipe(gulp.dest(paths.dist+'/fonts/'));
+});
+
+gulp.task('clean',function(done) {
+    $.del([paths.dist+'/',paths.tmp+'/'],done);
+});
+
+gulp.task('build',['images','bootstrapfonts']);
--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,6 @@ gulp.paths = {
 
 require('require-dir')('./gulp');
 
-gulp.task("default",function(){
-    gulp.start("build");
+gulp.task('default',['clean'], function(){
+    gulp.start('build');
 });
