fd2c2fced0b8c6b914c1b9a3f1f87b166848e42e
Marcio Nobrega <marcio.nobrega@sapo.pt>
2016-03-14 18:42:01 +0000

    New project structure.

--- gulp/build.js
+++ gulp/build.js
@@ -13,6 +13,10 @@ var $ = require('gulp-load-plugins')({
 //    gulp.src("bower_components/bootstrap/dist/css/**").pipe(gulp.dest("dist/css/"));
 //});
 
+gulp.task('html',['inject'],function() {
+
+});
+
 gulp.task('i18n',function() {
     //send to temp as it needs to be compiled
 });
@@ -21,7 +25,7 @@ gulp.task('images',function(){
     return gulp.src(paths.src+'/assets/img/**/*').pipe(gulp.dest(paths.dist+'/img/'));
 });
 
-gulp.task('bootstrapfonts',function() {
+gulp.task('fonts',function() {
     return gulp.src('bower_components/bootstrap/fonts/*.{eot,svg,ttf,woff,woff2}').pipe(gulp.dest(paths.dist+'/fonts/'));
 });
 
@@ -29,4 +33,4 @@ gulp.task('clean',function(done) {
     $.del([paths.dist+'/',paths.tmp+'/'],done);
 });
 
-gulp.task('build',['images','bootstrapfonts']);
+gulp.task('build',['html','images','fonts']);
