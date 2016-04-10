2cc940839125bca714bc79be893ad6f4f53abce0
Marcio Nobrega <marcio.nobrega@sapo.pt>
2016-04-10 09:45:07 +0100

    Started chapter 7.

--- src/app/app.js
+++ src/app/app.js
@@ -24,11 +24,18 @@ angular.module('inspinia', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', '
             templateUrl: "app/dashboard/dashboard_4.html",
             data: { pageTitle: 'Dashboard' }
         })
+
         .state('index.products',{
             url: "/products",
             templateUrl: "app/sportstore/products.tpl.html",
             data: { pageTitle: 'Sportstore Products'}
         })
+        .state('index.products.list',{
+            url: "/list",
+            templateUrl: "app/sportstore/products.list.tpl.html",
+            data : {}
+        })
+
         .state('index.minor', {
             url: "/minor",
             templateUrl: "app/minor/minor.html",
--- src/app/sportstore/products.js
+++ src/app/sportstore/products.js
@@ -1,13 +1,24 @@
 angular.module("inspinia")
-    .controller("ProductsCtrl", function($scope){
-        $scope.data = {
-            products: [
-                {name:"Product #1",description:"A Product", category:"Category #1", price:210},
-                {name:"Product #2",description:"A Product", category:"Category #1", price:220},
-                {name:"Product #3",description:"A Product", category:"Category #3", price:230},
-                {name:"Product #4",description:"A Product", category:"Category #2", price:250}
-            ]
-        }
+    .constant("dataUrl","http://localhost:2403/products")
+    .controller("ProductsCtrl", function($scope, $http, dataUrl){
+        //$scope.data = {
+        //    products: [
+        //        {name:"Product #1",description:"A Product", category:"Category #1", price:210},
+        //        {name:"Product #2",description:"A Product", category:"Category #1", price:220},
+        //        {name:"Product #3",description:"A Product", category:"Category #3", price:230},
+        //        {name:"Product #4",description:"A Product", category:"Category #2", price:250}
+        //    ]
+        //}
+
+        $scope.data = {};
+
+        $http.get(dataUrl)
+            .success(function(data){
+                $scope.data.products = data;
+            })
+            .error(function(error) {
+                $scope.data.error = error;
+            });
     })
     .constant("productListActiveClass","btn-primary")
     .constant("productListInactiveClass","btn-default")
