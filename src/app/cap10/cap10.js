b18bdf8570dcbcc78f2421fac3038c0a57e969a0
Marcio Nobrega <marcio.nobrega@sapo.pt>
2016-05-18 04:13:52 +0100

    Finished chapter 10. Directives. Data binding directives. Template directives.

--- src/app/cap10/cap10.js
+++ src/app/cap10/cap10.js
@@ -2,6 +2,12 @@ angular.module("cap10",["cap10.controllers"]);
 
 angular.module("cap10.controllers",[])
      .controller("defaultCtrl", function($scope) {
+
+         $scope.showList = false;
+         $scope.data = {};
+
+         $scope.radioOptions = ['None','Table','List'];
+
          $scope.todos = [
              {action: "Get groceries", complete: false},
              {action: "Call plumber", complete:false},
@@ -9,4 +15,12 @@ angular.module("cap10.controllers",[])
              {action: "Buy flowers",complete:false},
              {action: "Call family",complete:false}
          ];
+
+         $scope.viewFile = function() {
+             return $scope.showList ? "app/cap10/cap10.todosList.tpl.html":"app/cap10/cap10.todosListV2.tpl.html";
+         }
+
+         $scope.reportChange = function() {
+             console.log("Displayed content: "+$scope.viewFile());
+         }
     });
