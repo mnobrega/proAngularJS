263c044d6ec6bb6ac82bfd5defa84786f4d40f7a
Marcio nobrega <marcio.nobrega@sapo.pt>
2016-08-22 00:34:53 +0100

    Finished chapter 12.

--- src/app/cap12/cap12.js
+++ src/app/cap12/cap12.js
@@ -10,6 +10,18 @@ angular.module("cap12.controllers",[])
             {action: "Call family",complete:false}
         ];
 
+        $scope.selectOptions = [
+            {id:100, action:"Get Groceries", complete:false},
+            {id:200, action:"Call plumber", complete:false},
+            {id:300, action:"Buy running shoes",complete:true}
+        ];
+
+        $scope.selectOptions2 = [
+            {id:100, place:"Store",action:"Get groceries",complete:false},
+            {id:200, place:"Home",action:"Call plumber",complete:false},
+            {id:300, place:"Store",action:"Buy running shoes",complete:true}
+        ];
+
         $scope.addNewItem = function(newItem) {
             if (angular.isDefined(newItem) && angular.isDefined(newItem.action)
                     && angular.isDefined(newItem.location)) {
@@ -20,11 +32,30 @@ angular.module("cap12.controllers",[])
             }
         };
 
-
         $scope.addUser = function(userDetails) {
             $scope.message = userDetails.name + " ("+userDetails.email+") ("+userDetails.agreed+")";
         };
 
+        $scope.addUserWithValidation = function(userDetails) {
+            if ($scope.chap12Form2.$valid) {
+                $scope.message = userDetails.name + " ("+userDetails.email+") ("+userDetails.agreed+")";
+            } else {
+                $scope.showValidation = true;
+            }
+        }
 
         $scope.message = "Ready";
+
+        $scope.getError = function (error) {
+            if (angular.isDefined(error)) {
+                if (error.required) {
+                    return "Please enter a value";
+                } else if (error.email) {
+                    return "Please enter a valid email address";
+                }
+            }
+        }
+
+        $scope.requireValue = true;
+        $scope.matchPattern = new RegExp("^[a-z]");
     });
