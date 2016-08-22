ed3fcdad9dd0d1925699d6a0944ee3660a9b7446
Marcio nobrega <marcio.nobrega@sapo.pt>
2016-08-22 17:00:26 +0100

    Continue chapter 13. At page 341

--- src/app/cap13/cap13.js
+++ src/app/cap13/cap13.js
@@ -1,6 +1,43 @@
 angular.module("cap13",["cap13.controllers"]);
 
 angular.module("cap13.controllers",["cap13.services"])
+    .controller("topLevelCtrl", function($scope) {
+
+        $scope.data = {
+            dataValue: "Hello, Adam"
+        };
+
+        $scope.reverseText = function() {
+            $scope.data.dataValue = $scope.data.dataValue.split("").reverse().join("");
+        };
+
+        $scope.changeCase = function() {
+            var result = [];
+            angular.forEach($scope.data.dataValue.split(""),function(char, index) {
+                result.push(index % 2 == 1
+                    ? char.toString().toUpperCase() : char.toString().toLowerCase());
+            });
+            $scope.data.dataValue = result.join("");
+        };
+    })
+    .controller("firstChildCtrl", function($scope) {
+        $scope.changeCase = function() {
+            $scope.data.dataValue = $scope.data.dataValue.toUpperCase();
+        };
+    })
+    .controller("secondChildCtrl", function($scope) {
+        $scope.changeCase = function() {
+            $scope.data.dataValue = $scope.data.dataValue.toLowerCase();
+        };
+
+        $scope.shiftFour = function () {
+            var result = [];
+            angular.forEach($scope.data.dataValue.split(""), function(char, index) {
+                result.push(index < 4 ? char.toUpperCase():char);
+            });
+            $scope.data.dataValue = result.join("");
+        };
+    })
     .controller("simpleCtrl", function($scope){
         $scope.cities = ["London","New York","Paris"];
 
@@ -24,24 +61,24 @@ angular.module("cap13.controllers",["cap13.services"])
         };
 
         $scope.copyAddress = function() {
-            console.log($scope.shippingZip);
             $scope.shippingZip = $scope.billingZip;
-            console.log($scope.shippingZip);
         };
     })
-    .controller("reusedController", function($scope, $rootScope) {
+    .controller("reusedController", function($scope, ZipCodes) {
         $scope.$on("zipCodeUpdated", function(event, args) {
             $scope[args.type] = args.zipCode;
+            console.log(args);
+            console.log($scope[args.type]);
         });
 
         $scope.setAddress = function (type, zip) {
-            ZipCodes.setZipCode(type,zip);
             console.log("Type: " + type + " " + zip);
+            ZipCodes.setZipCode(type,zip);
         };
 
         $scope.copyAddress = function() {
-            $scope.zip = $scope.billingZip;
-            console.log($scope.shippingZip);
+            console.log($scope.billingZip2);
+            $scope.shippingZip2 = $scope.billingZip2;
         };
     });
 
