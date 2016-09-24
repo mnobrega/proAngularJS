f1db7b3204b032743f391cf2d0ddab89e8434016
Marcio nobrega <marcio.nobrega@sapo.pt>
2016-09-24 12:51:29 +0100

    Continue chapter 16. At page 441

--- src/app/cap16/cap16.js
+++ src/app/cap16/cap16.js
@@ -1,8 +1,11 @@
 angular.module("cap16",["cap16.controllers","cap16.directives"]);
 
 angular.module("cap16.controllers",[])
+    .controller("cap16Ctrl5", function($scope) {
+        $scope.data = {name:"Marcio"};
+    })
     .controller("cap16Ctrl4", function($scope) {
-        $scope.data = {name: "Addam"};
+        $scope.data = {name: "Adam"};
         $scope.city = "London";
     })
     .controller("cap16Ctrl2", function($scope) {
@@ -20,6 +23,26 @@ angular.module("cap16.controllers",[])
     });
 
 angular.module("cap16.directives",[])
+    .directive("scopeDemo2IsolatedTwoWay", function() {
+        return {
+            template: function() {
+                return angular.element(document.querySelector("scopeTemplate2WayBinding")).html();
+            },
+            scope: {
+                local: "=nameprop"
+            }
+        }
+    })
+    .directive("scopeDemo2IsolatedBinding", function() {
+        return {
+            template: function() {
+                return angular.element(document.querySelector("#scopeTemplateBinding")).html();
+            },
+            scope : {
+                local: "@nameprop"
+            }
+        }
+    })
     .directive("scopeDemo2Isolated", function() {
         return {
             template: function() {
@@ -38,7 +61,7 @@ angular.module("cap16.directives",[])
     })
     .directive("scopeDemoWithScope", function() {
         return {
-            template: "<div class='panel-body'>Name: <input ng-model=name /></div>",
+            template: "<div class='panel-body'>NameWithScope: <input ng-model=name /></div>",
             scope: true
         }
     })
