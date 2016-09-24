7075f14e047debcecad363abc452360f17259b04
Marcio nobrega <marcio.nobrega@sapo.pt>
2016-09-24 14:39:51 +0100

    Started chapter 17.

---
+++ src/app/cap17/cap17.js
@@ -0,0 +1,10 @@
+angular.module("cap17",["cap17.controllers","cap17.directives"]);
+
+angular.module("cap17.controllers",[]);
+
+angular.module("cap17.directives",[])
+    .directive("cap17Panel", function() {
+        return {
+
+        }
+    });
