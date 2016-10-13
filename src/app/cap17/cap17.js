32d39b773a4b142c20c7c153394fb122cab97d83
Marcio Nobrega <marcio.nobrega@sapo.pt>
2016-10-13 01:10:01 +0100

    Finished chapter 17.

--- src/app/cap17/cap17.js
+++ src/app/cap17/cap17.js
@@ -1,6 +1,9 @@
 angular.module("cap17",["cap17.controllers","cap17.directives"]);
 
 angular.module("cap17.controllers",[])
+    .controller("cap17Ctrl4", function($scope) {
+        $scope.dataValue = "Not Sure";
+    })
     .controller("cap17Ctrl3", function($scope) {
         $scope.products = [
             {name: "Apples", price: 1.20, quantity: 2},
@@ -27,9 +30,77 @@ angular.module("cap17.controllers",[])
     });
 
 angular.module("cap17.directives",[])
+    .directive("triButton", function() {
+        return {
+            restrict: "E",
+            replace: true,
+            require: "ngModel",
+            template: angular.element(document.querySelector("#cap17TriTemplate")).html(),
+            link: function (scope, element, attrs, ctrl) {
+
+                var validateParser = function(value){
+                    var valid = (value == "Yes" || value == "No");
+                    ctrl.$setValidity("confidence",valid);
+                    return valid ? value : undefined;
+                };
+
+                ctrl.$parsers.push(validateParser);
+
+                element.on("click", function(event) {
+                    setSelected(event.target.innerText);
+                    scope.$apply(function () {
+                        ctrl.$setViewValue(event.target.innerText);
+                    });
+                });
+
+                var setSelected = function(value) {
+                    var buttons = element.find("button");
+                    buttons.removeClass("btn-primary");
+                    for (var i = 0; i < buttons.length; i++) {
+                        if (buttons.eq(i).text() == value) {
+                            buttons.eq(i).addClass("btn-primary");
+                        }
+                    }
+                };
+
+                ctrl.$formatters.push(function (value) {
+                   return value == "Huh?" ? "Not Sure" : value;
+                });
+
+                ctrl.$render = function() {
+                    setSelected(ctrl.$viewValue || "Not Sure");
+                }
+
+            }
+        }
+    })
+    .directive("resetTotals", function() {
+        return {
+            scope: {
+                data: "=productData",
+                propname: "@propertyName"
+            },
+            template: angular.element(document.querySelector("#cap17ResetTemplate")).html(),
+            require: "^productTable", /* use productTable */
+            link: function (scope, element, attrs, ctrl) {
+                scope.reset = function() {
+                    for (var i = 0; i < scope.data.length; i++) {
+                        scope.data[i][scope.propname] = 0;
+                    }
+                    ctrl.updateTotal();
+                }
+            }
+        }
+    })
     .directive("productItem", function() {
         return {
-            template: angular.element(document.querySelector("#cap17ProductTemplate")).html()
+            template: angular.element(document.querySelector("#cap17ProductTemplate")).html(),
+            require: "^productTable", /* use productTable */
+            link: function (scope, element, attrs, ctrl) {
+                scope.$watch("item.quantity", function() {
+                    ctrl.updateTotal();
+                });
+            }
         }
     })
     .directive("productTable",function() {
@@ -39,6 +110,15 @@ angular.module("cap17.directives",[])
                 value: "=productTable",
                 data: "=productData"
             },
+            controller: function ($scope, $element, $attrs) {
+                this.updateTotal = function() {
+                    var total = 0;
+                    for (var i=0; i < $scope.data.length; i++) {
+                        total += Number($scope.data[i].quantity);
+                    }
+                    $scope.value = total;
+                }
+            }
         }
     })
     .directive("panel", function() {
